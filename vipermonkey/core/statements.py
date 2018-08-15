87e36abb2ee37c8a84e73a1a7db6c5a175b752a0
kirk-sayre-work <Kirk.Sayre@walmart.com>
2018-08-15 12:14:41 -0500

    Handle calls like Shell#().

--- vipermonkey/core/statements.py
+++ vipermonkey/core/statements.py
@@ -1517,6 +1517,8 @@ class Call_Statement(VBA_Object):
             self.name = str(self.name).replace("@", "")
         if (str(self.name).endswith("!")):
             self.name = str(self.name).replace("!", "")
+        if (str(self.name).endswith("#")):
+            self.name = str(self.name).replace("#", "")
         self.params = tokens.params
         log.debug('parsed %r' % self)
 
