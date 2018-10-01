bd79d965186666f133f70cafba76ccc6e216b73f
kirk-sayre-work <Kirk.Sayre@walmart.com>
2018-10-01 11:21:15 -0500

    Added KeyString VBA function.

--- vipermonkey/core/vba_library.py
+++ vipermonkey/core/vba_library.py
@@ -1525,7 +1525,175 @@ class CurDir(VbaLibraryFunc):
 
     def eval(self, context, params=None):
         return "~"
-            
+
+class KeyString(VbaLibraryFunc):
+    """
+    KeyString() function.
+    """
+
+    def eval(self, context, params=None):
+
+        # Key string value map.
+        key_vals = {
+            1 : "Left Button",
+            2 : "Right Button",
+            3 : "Cancel",
+            4 : "Middle Button",
+            8 : "Backspace",
+            9 : "Tab",
+            12 : "Clear (Num 5)",
+            13 : "Return",
+            16 : "Shift",
+            17 : "Control",
+            18 : "Alt",
+            19 : "Pause",
+            20 : "Caps Lock",
+            27 : "Esc",
+            32 : "Space",
+            33 : "Page Up",
+            34 : "Page Down",
+            35 : "End",
+            36 : "Home",
+            37 : "Left",
+            38 : "Up",
+            39 : "Right",
+            40 : "Down",
+            41 : "Not Avail",
+            42 : "Not Avail",
+            43 : "Not Avail",
+            44 : "Print Screen",
+            45 : "Insert",
+            46 : "Del",
+            47 : "Not Avail",
+            48 : "0",
+            49 : "1",
+            50 : "2",
+            51 : "3",
+            52 : "4",
+            53 : "5",
+            54 : "6",
+            55 : "7",
+            56 : "8",
+            57 : "9",
+            65 : "A",
+            66 : "B",
+            67 : "C",
+            68 : "D",
+            69 : "E",
+            70 : "F",
+            71 : "G",
+            72 : "H",
+            73 : "I",
+            74 : "J",
+            75 : "K",
+            76 : "L",
+            77 : "M",
+            78 : "N",
+            79 : "O",
+            80 : "P",
+            81 : "Q",
+            82 : "R",
+            83 : "S",
+            84 : "T",
+            85 : "U",
+            86 : "V",
+            87 : "W",
+            88 : "X",
+            89 : "Y",
+            90 : "Z",
+            96 : "Num 0",
+            97 : "Num 1",
+            98 : "Num 2",
+            99 : "Num 3",
+            100 : "Num 4",
+            101 : "Num 5",
+            102 : "Num 6",
+            103 : "Num 7",
+            104 : "Num 8",
+            105 : "Num 9",
+            106 : "Num *",
+            107 : "Num +",
+            108 : "Not Avail",
+            109 : "Num -",
+            110 : "Num .",
+            111 : "Num /",
+            112 : "F1",
+            113 : "F2",
+            114 : "F3",
+            115 : "F4",
+            116 : "F5",
+            117 : "F6",
+            118 : "F7",
+            119 : "F8",
+            120 : "F9",
+            121 : "F10",
+            122 : "F11",
+            123 : "F12",
+            124 : "F13",
+            125 : "F14",
+            126 : "F15",
+            127 : "F16",
+            128 : "F17",
+            129 : "F18",
+            130 : "F19",
+            131 : "F20",
+            132 : "F21",
+            133 : "F22",
+            134 : "F23",
+            135 : "F24",
+            144 : "Num Lock",
+            145 : "Scroll Lock",
+            160 : "Shift",
+            161 : "Shift",
+            162 : "Ctrl",
+            163 : "Ctrl",
+            164 : "Alt",
+            165 : "Alt",
+            172 : "M",
+            173 : "D",
+            174 : "C",
+            175 : "B",
+            176 : "P",
+            177 : "Q",
+            178 : "J",
+            179 : "G",
+            183 : "F",
+            186 : ";",
+            187 : "=",
+            188 : ",",
+            189 : "-",
+            190 : ".",
+            191 : "/",
+            192 : "`",
+            194 : "F15",
+            219 : "[",
+            220 : "\\",
+            221 : "]",
+            222 : "'",
+            226 : "\\"
+        }
+
+        v1 = None
+        v2 = None
+        try:
+            v1 = int(params[0])
+            if (len(params) >= 2):
+                v2 = int(params[1])
+        except Exception as e:
+            log.error("KeyString: Invalid args " + str(params) + ". " + str(e))
+            return ""
+
+        r = ""
+        if (v1 in key_vals):
+            r += key_vals[v1]
+        if (v2 is not None):
+            r += ","
+            if (v2 in key_vals):
+                key_vals[v2]
+
+        log.debug("KeyString: args = " + str(params) + ", return " + r)
+        return r
+        
 class Run(VbaLibraryFunc):
     """
     Application.Run() function.
@@ -1845,7 +2013,8 @@ for _class in (MsgBox, Shell, Len, Mid, MidB, Left, Right,
                CheckSpelling, Specialfolders, StrComp, Space, Year, Variable,
                Exec, CDbl, Print, CreateTextFile, Write, Minute, Second, WinExec,
                CallByName, ReadText, Variables, Timer, Open, CVErr, WriteLine,
-               URLDownloadToFile, FollowHyperlink, Join, VarType, DriveExists, Navigate):
+               URLDownloadToFile, FollowHyperlink, Join, VarType, DriveExists, Navigate,
+               KeyString):
     name = _class.__name__.lower()
     VBA_LIBRARY[name] = _class()
 
