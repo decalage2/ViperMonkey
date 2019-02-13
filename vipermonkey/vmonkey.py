b86fd8406cf2fd6e9f7e14aaf87a168bc4b32b46
Erik Fichtner <emf@obfuscation.org>
2019-02-13 09:26:38 -0600

    BUGFIX:  Must be EXACTLY ONE space!
    " The underscore must be immediately preceded by a space and immediately followed by a line terminator (carriage return)."
    https://docs.microsoft.com/en-us/dotnet/visual-basic/programming-guide/program-structure/how-to-break-and-combine-statements-in-code

--- vipermonkey/vmonkey.py
+++ vipermonkey/vmonkey.py
@@ -807,7 +807,7 @@ def strip_useless_code(vba_code, local_funcs):
     """
 
     # Clear out lines broken up on multiple lines.
-    #vba_code = re.sub(r"_\s*\n", "", vba_code)
+    vba_code = re.sub(r"_\s\n", "", vba_code)
     vba_code = re.sub(r":\s*[Ee]nd\s+[Ss]ub", r"\nEnd Sub", vba_code)
     
     # Track data change callback function names.
