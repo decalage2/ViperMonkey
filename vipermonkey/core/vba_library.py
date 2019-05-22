c08f8aa344f954adf705128023034b35b85346ed
kirk-sayre-work <Kirk.Sayre@walmart.com>
2019-05-22 09:32:00 -0500

    Support getting doc text with ActiveDocument.Range.Text field references.

--- vipermonkey/core/vba_library.py
+++ vipermonkey/core/vba_library.py
@@ -482,6 +482,12 @@ class Execute(VbaLibraryFunc):
 
     def eval(self, context, params=None):
 
+        # Sanity check.
+        if ((len(params) == 0) or
+            (isinstance(params[0], VBA_Object)) or
+            (isinstance(params[0], VbaLibraryFunc))):
+            return "NULL"
+        
         # Save the command.
         command = strip_nonvb_chars(str(params[0]))
         # Why am I doing this?
