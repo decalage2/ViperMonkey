bb22efbdafa1362a97877b67b2e7159da416c0d2
unk0unk0unk0 <51192060+unk0unk0unk0@users.noreply.github.com>
2019-05-30 17:52:02 +0900

    Add files via upload

--- vipermonkey/core/__init__.py
+++ vipermonkey/core/__init__.py
@@ -398,6 +398,8 @@ class ViperMonkey(object):
 
         # Save the document text in the proper variable in the context.
         context.globals["ActiveDocument.Content.Text".lower()] = "\n".join(self.doc_text)
+        context.globals["ActiveDocument.Content.Start".lower()] = 0
+        context.globals["ActiveDocument.Content.End".lower()] = len("\n".join(self.doc_text))
         context.globals["ActiveDocument.Paragraphs".lower()] = self.doc_text
         
         # reset the actions list, in case it is called several times
