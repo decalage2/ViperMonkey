f783f19b3507e4522c24d8db54ec8bd8294a7d46
kirk-sayre-work <Kirk.Sayre@walmart.com>
2019-09-14 16:35:25 -0500

    Automatically start soffice.bin if needed when exporting Excel sheets to CSV. Does not work properly right now.

--- vipermonkey/export_all_excel_sheets.py
+++ vipermonkey/export_all_excel_sheets.py
@@ -5,6 +5,9 @@
 
 import sys
 import os
+# sudo pip3 install psutil
+import psutil
+import subprocess
 
 # sudo pip3 install unotools
 from unotools import Socket, connect
@@ -14,6 +17,37 @@ from unotools.unohelper import convert_path_to_url
 # Connection information for LibreOffice.
 HOST = "localhost"
 PORT = 2002
+
+def is_excel_file(maldoc):
+    """
+    Check to see if the given file is an Excel file..
+
+    @param name (str) The name of the file to check.
+
+    @return (bool) True if the file is an Excel file, False if not.
+    """
+    typ = subprocess.check_output(["file", maldoc])
+    return (b"Excel" in typ)
+
+def run_soffice():
+
+    # Is soffice already running?
+    for proc in psutil.process_iter():
+        try:
+            pinfo = proc.as_dict(attrs=['pid', 'name', 'username'])
+        except psutil.NoSuchProcess:
+            pass
+        else:
+            if (pinfo["name"].startswith("soffice")):
+
+                # Already running. Don't start it again.
+                return
+
+    # soffice is not running. Run it in listening mode.
+    cmd = ["/usr/lib/libreoffice/program/soffice.bin", "--headless", "--invisible",
+           "--nocrashreport", "--nodefault", "--nofirststartwizard", "--nologo",
+           "--norestore", '--accept="socket,host=127.0.0.1,port=2002,tcpNoDelay=1;urp;StarOffice.ComponentContext"']
+    subprocess.Popen(cmd)
 
 def get_component(fname, context):
     """
@@ -31,6 +65,18 @@ def convert_csv(fname):
     return - A list of the names of the CSV sheet files.
     """
 
+    # Make sure this is an Excel file.
+    if (not is_excel_file(fname)):
+
+        # Not Excel, so no sheets.
+        return []
+
+    # Run soffice in listening mode if it is not already running.
+    run_soffice()
+    
+    # TODO: Make sure soffice is running in listening mode.
+    # 
+    
     # Connect to the local LibreOffice server.
     context = connect(Socket(HOST, PORT))
 
@@ -64,6 +110,9 @@ def convert_csv(fname):
     # Close the spreadsheet.
     component.close(True)
 
+    # Kill soffice after done?
+    # /usr/lib/libreoffice/program/soffice.bin --headless --invisible --nocrashreport --nodefault --nofirststartwizard --nologo --norestore --accept=socket,host=127.0.0.1,port=2002,tcpNoDelay=1;urp;StarOffice.ComponentContext
+    
     # Done.
     return r
 
