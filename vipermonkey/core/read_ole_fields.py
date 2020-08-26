ebda5103ea08d07ebb7b97b0f2031f939dd30e01
Kirk.Sayre <Kirk.Sayre@walmart.com>
2020-08-26 09:19:44 -0500

    More ActiveX.

--- vipermonkey/core/read_ole_fields.py
+++ vipermonkey/core/read_ole_fields.py
@@ -1195,7 +1195,7 @@ def get_ole_text_method_1(vba_code, data):
               r' {1,10}Caption {1,30}= {1,30}"\w{1,20}"\r\n' + \
               r' {1,10}ClientHeight {1,30}= {1,30}\d{1,20}\r\n' + \
               r' {1,10}ClientLeft {1,30}= {1,30}\d{1,20}\r\n' + \
-              r' {1,10}ClientTop {1,30}= {1,30}\d{3}'
+              r' {1,10}ClientTop {1,30}= {0,30}(?:\d{3})?'
     for val in tmp_vals:
 
         # Ignore empty strings.
