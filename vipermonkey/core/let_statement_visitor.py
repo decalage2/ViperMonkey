db3704113c294a32dcb0b18ea410782b1d25e102
Kirk.Sayre <Kirk.Sayre@walmart.com>
2021-04-05 09:54:14 -0500

    Code cleanup

--- vipermonkey/core/let_statement_visitor.py
+++ vipermonkey/core/let_statement_visitor.py
@@ -36,7 +36,7 @@ https://github.com/decalage2/ViperMonkey
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from visitor import *
+from visitor import visitor
 
 class let_statement_visitor(visitor):
     """
